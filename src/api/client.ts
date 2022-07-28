5b810b19946a061aae4e1dcffcc65476c3920fb0
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-07-28 14:10:47 +0900

    type error

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,4 +3,3 @@ import axios from 'axios'
 export default axios.create({
   baseURL: process.env.VUE_APP_API_BASE
 })
-
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
   },
   {
     path: '/about',
@@ -13,7 +19,8 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: 'about' */ '../views/AboutView.vue')
   }
 ]
 
